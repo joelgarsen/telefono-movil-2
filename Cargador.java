efe352d17c08c77ea8a13f60260ad66550050285
Pablo <pablocg7518@gmail.com>
2024-02-19 10:33:44 +0100

    Commit 3

---
+++ Cargador.java
@@ -0,0 +1,12 @@
+public class Cargador
+{
+    private String tipoDeConector;
+    private int corrienteSuministrada;
+    private Cable clavija;
+
+    public Cargador(String tipoDeConector, int corrienteSuministrada) {
+        this.tipoDeConector = tipoDeConector;
+        this.corrienteSuministrada = corrienteSuministrada;
+    }
+    
+}
