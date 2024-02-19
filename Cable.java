d2f4a790cc8bdd478e79f6ec978199d84761bba8
Pablo <pablocg7518@gmail.com>
2024-02-19 10:19:47 +0100

    Commit 2

---
+++ Cable.java
@@ -0,0 +1,30 @@
+public class Cable {
+   public int longitud;
+   public String tipoDeRecubrimiento;
+   
+   public Cable (int longitud, String tipoDeRecubrimiento){
+       this.longitud = longitud;
+       this.tipoDeRecubrimiento = tipoDeRecubrimiento;
+   }
+   
+   public int getLongitud() {
+        return longitud;
+   } 
+    
+   public String getTipoDeRecubrimiento() {
+       return tipoDeRecubrimiento;
+   }
+   
+   public void setMarca(int newLongitud) {
+       longitud = newLongitud;
+   }
+   
+   public void setTipoDeRecubrimiento(String newTipoDeRecubrimiento) {
+       tipoDeRecubrimiento = newTipoDeRecubrimiento;
+   }
+   
+   public void imprimirDetalles(){
+       System.out.println("Longitud: " + longitud + " cm");
+       System.out.println("Tipo de recubrimiento: " + tipoDeRecubrimiento);
+   }
+}
