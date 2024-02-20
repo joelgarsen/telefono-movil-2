8f43564401c5bda848599d8cabf84286db70d089
Joelgarsen <joelgsen999@gmail.com>
2024-02-20 10:54:06 +0100

    Commit 4

--- Cargador.java
+++ Cargador.java
@@ -9,4 +9,24 @@ public class Cargador
         this.corrienteSuministrada = corrienteSuministrada;
     }
     
+    public String getTipoDeConector(){
+    	return tipoDeConector;
+    }
+    
+    public int getCorrienteSuministrada() {
+    	return corrienteSuministrada;
+    }
+    
+    public void setTipoDeConector(String newTipoDeConector) {
+    	tipoDeConector = newTipoDeConector;
+    }
+    
+    public void setCorrienteSuministrada(int newCorrienteSuministrada) {
+    	corrienteSuministrada = newCorrienteSuministrada;
+    }
+    
+    public void imprimirDetalles(){
+           System.out.println("CorrienteSuministrada: " + corrienteSuministrada + " A ");
+           System.out.println("Tipo de conector: " + tipoDeConector);
+    }
 }
